ea85c2203052993e3f24e63b4c77f416465100ae
Dema98HH <w.demenkow@gmail.com>
2022-08-19 13:01:21 +0200

    Received with props

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -2,16 +2,24 @@
   <button class="btn btn-danger" @click="count++">
     You clicked me {{ count }} times.
   </button>
+  <div>
+    Received: {{message}}
+  </div>
 </template>
+
 
 
 <script>
 
 export default {
+
   data() {
     return {
-      count: 0
+      count: 0,
     }
-  }
+  },
+
+props: ['message']
+
 }
 </script>
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -2,18 +2,34 @@
 
 <template>
   <h1>Here is a child component!</h1>
-  <ButtonCounter />
-
+  <textarea type="text" v-model="text" />
+  <button @click="send()">Click me!</button>
+  <Child :message='message' class="mt-10"></Child>
 </template>
+  <Child />
 
 
 <script>
 
-import ButtonCounter from '/home/dema/PycharmProjects/app/frontend/src/components/HelloWorld.vue'
+import Child from '/home/dema/PycharmProjects/app/frontend/src/components/HelloWorld.vue'
   
 export default {
   components: {
-    ButtonCounter
+    Child
+  },
+
+  methods: {
+    send(){
+      this.message = this.text
+      this.text = ''
+    },
+  },
+
+  data(){
+    return{
+      text: '',
+      message: '',
+    }
   }
 }
 </script>
