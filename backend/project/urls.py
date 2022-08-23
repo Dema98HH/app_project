82c7e40490a1e587a1491f885a44d02d219a90b0
Dema98HH <w.demenkow@gmail.com>
2022-08-23 08:11:56 +0200

    home site with students is created (APIView)

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('students.urls')),
+    path('api/', include('students.urls')),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/', include('students.urls')),
